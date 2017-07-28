b8bd0db6f7997066b2c88415f332e36d1cc18251
Eli Nicholson <efn3@hood.edu>
2017-07-28 12:20:18 -0400

    added development information

--- ui.R
+++ ui.R
@@ -302,8 +302,13 @@ shinyUI(
                                       p(em("Community Resilience Economic Decision Guide for Buildings and Infrastructure Systems (EDG):"),
                                       a("http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.1197.pdf", href = "https://www.nist.gov/topics/community-resilience")),
                                       p(em("Community Resilience Economic Guide Brochure: "),
-                                      a("https://www.nist.gov/sites/default/files/documents/2017/01/23/nist_communityresilience_brochure-11b_finaldec2016.pdf", href = "https://www.nist.gov/topics/community-resilience"))
-                                      
+                                      a("https://www.nist.gov/sites/default/files/documents/2017/01/23/nist_communityresilience_brochure-11b_finaldec2016.pdf", href = "https://www.nist.gov/topics/community-resilience")),
+                                      br(),
+                                      br(),
+                                      h3("Statement of Development", style = "color:#2471A3"),
+                                      hr(style = "color:#2471A3"),
+                                      p("This software was developed by employees of the National Institute of Standards and Technology (NIST), an agency of the Federal Government. Pursuant to title 17 United States Code Section 105, works of NIST employees are not subject to copyright protection in the United States and are considered to be in the public domain. Permission to freely use, copy, modify, and distribute this software and its documentation without fee is hereby granted, provided that this notice and disclaimer of warranty appears in all copies."),
+                                      p("THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE. IN NO EVENT SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY, CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY PERSONS OR PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.")                                  
                                       )
                                     )
                       ),
